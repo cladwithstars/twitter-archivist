a75a065955140bbad670ecb342baf58ae1258cf6
Will Morgan <morgan88will@gmail.com>
2022-07-19 23:45:22 -0400

    savetweetform work

--- client/src/components/SaveTweetForm.tsx
+++ client/src/components/SaveTweetForm.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  TextField,
+  Button,
+  FormControl,
+  FormLabel,
+  FormControlLabel,
+} from "@mui/material";
 
 const SaveTweetForm = () => {
-  return <div>SaveTweetForm</div>;
+  const [id, setId] = useState("");
+  const handleSubmit = () => {};
+  return (
+    <div>
+      <TextField
+        id="outlined-basic"
+        label="Save Tweet By Id"
+        variant="outlined"
+        required
+        value={id}
+        onChange={(e) => setId(e.target.value)}
+      />
+      <Button variant="outlined" onClick={handleSubmit}>
+        Save Tweet
+      </Button>
+    </div>
+  );
 };
 
 export default SaveTweetForm;
