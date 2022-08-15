23e6a01d594074af8835e9a07b7e9d4bfc75ec7e
Will Morgan <morgan88will@gmail.com>
2022-08-15 18:12:06 -0400

    .

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(__dirname + "/client/build/index.html");
   });
 
-  app.get("/folder/*", (req, res) => {
-    res.redirect("/");
-  });
+  // app.get("/folder/*", (req, res) => {
+  //   res.redirect("/");
+  // });
 
   app.use("", express.static(__dirname + "/client/build/"));
 }
