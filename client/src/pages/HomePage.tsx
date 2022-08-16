45d8b4d4225d2789514a760c4c27fad14f07f5e8
Will Morgan <morgan88will@gmail.com>
2022-08-16 17:27:48 -0400

    .

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import SaveTweetForm from "../components/SaveTweetForm/SaveTweetForm";
 import MyFolders from "../components/MyFolders/MyFolders";
 import CreateFolder from "../components/CreateFolder/CreateFolder";
 import { Container } from "@mui/material";
+import AuthContext from "../context/auth/authContext";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+  const authContext = useContext(AuthContext);
+  const { isAuthenticated } = authContext;
+  if (!isAuthenticated) {
+    navigate("/login");
+  }
   return (
     <Container sx={{ width: "95%" }}>
       <SaveTweetForm />
