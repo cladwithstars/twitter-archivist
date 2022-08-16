a17c10003ea694e9c82b02b2a12d51b43ef1099b
Will Morgan <morgan88will@gmail.com>
2022-08-16 17:09:07 -0400

    .

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use("/api/tweets", require("./routes/tweets"));
 if (process.env.NODE_ENV === "production") {
   app.use("", express.static(__dirname + "/client/build/"));
 
-  app.get("", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(__dirname + "/client/build/index.html");
   });
 }
